326c96d8d7bd8b77873d56722abafaa299133e48
mattvvhat <not.mattowen@gmail.com>
2015-02-18 23:06:03 -0500

    :fallen_leaf:

--- src/image_color/k-colors.clj
+++ src/image_color/k-colors.clj
@@ -9,6 +9,10 @@
             [clojure.java.io :as io]))
 
 
+;; k - c o l o r
+;; k - c o l o r
+;; k - c o l o r
+
 (defn k-color [buffered-image k]
   "
   Return k-clusters of RGB values as Vectors
@@ -17,3 +21,43 @@
   @return k-length vector of RGB hash-maps
   "
   [{:r 0 :g 0 :b 0}])
+
+;; p i x e l - c o l o r
+;; p i x e l - c o l o r
+;; p i x e l - c o l o r
+
+(defn pixel-color [buffered-image x y]
+  "
+  Return the RGB-value of a pixel at xy-coordinate
+  @param buffered-image BufferedImage
+  @param x integer
+  @param y integer 
+  "
+  {:r 0 :g 0 :b 0})
+
+;; r a n d o m - p o i n t
+;; r a n d o m - p o i n t
+;; r a n d o m - p o i n t
+
+(defn random-point [buffered-image]
+  [(rand-int 0 (.getWidth buffered-image))
+   (rand-int 0 (.getHeight buffered-image))])
+
+;; r a n d o m - s a m p l e - p i x e l s
+;; r a n d o m - s a m p l e - p i x e l s
+;; r a n d o m - s a m p l e - p i x e l s
+
+(defn random-sample-pixels [buffered-image n]
+  "
+  Randomly Sample n-points for Their Color Value
+  @param buffered-image
+  @param n integer number of points to sample
+  "
+  (map (fn [x] (random-point buffered-image)) (range n)))
+
+;; r a n d o m - s a m p l e - c o l o r s 
+;; r a n d o m - s a m p l e - c o l o r s 
+;; r a n d o m - s a m p l e - c o l o r s 
+
+(defn random-sample-colors [buffered-image n]
+  (map (fn [x] [1 2]) (random-sample-pixels buffered-image n)))
---
+++ test/image_color/k_colors_test.clj
@@ -0,0 +1,4 @@
+(ns image-color.k-colors-test
+  (:require [clojure.math.numeric-tower :as math]
+            [clojure.java.io :as io]
+            [image-color.k-colors :refer :all]))
